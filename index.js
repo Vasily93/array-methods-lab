8786a17d7b6a683cae6798fb1a171f1374a9871f
Vasily93 <markovavasily@gmail.com>
2019-06-25 08:47:28 -0700

    in the process

---
+++ index.js
@@ -0,0 +1,34 @@
+const nums = [1, 2, 3, 4, 45];
+//map function
+ 
+function map(arr, callback) {
+    const maped = [];
+    for( let i = 0; i < arr.length; i++) {
+        maped[i] = callback(arr[i])
+    }
+    return maped;
+}
+    
+//filter function
+function filter(arr, callback) {
+    const filtered = [];
+    for( let i = 0; i < arr.length; i++) {
+        filtered[i] = callback(arr[i])
+    }
+    return filtered;
+//find index function
+function forInfdex(arr, callback) {
+    for( let i = 0; i < arr.length; i++) {
+        const firstIndex = Number
+        
+    } 
+}
+//reduce function
+
+//every function
+
+
+module.exports = { 
+    map,
+
+}
